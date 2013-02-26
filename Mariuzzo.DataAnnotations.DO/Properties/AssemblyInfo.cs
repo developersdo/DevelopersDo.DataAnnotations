010ac06ab64c97a77a1f3886126eb61a34e04035
Rubens Mariuzzo <rubens@mariuzzo.com>
2013-02-26 09:38:52 -0400

    Initial project setup.

---
+++ Mariuzzo.DataAnnotations.DO/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Mariuzzo.DataAnnotations.DO")]
+[assembly: AssemblyDescription("Provides common validation attributes for Dominican Republic (DO).")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mariuzzo")]
+[assembly: AssemblyProduct("Mariuzzo.DataAnnotations.DO")]
+[assembly: AssemblyCopyright("Copyright © Rubens Mariuzzo 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("6c9432cd-b9f6-4e61-817c-8fc40f3ce4a5")]
+[assembly: AssemblyVersion("13.2.26.0")]
+[assembly: AssemblyFileVersion("13.2.26.0")]
